6affb9291c0b89561a3e176c28ad661c92db6caf
sun-fox <1999sunnykumarsingh@gmail.com>
2020-02-07 23:41:44 +0530

    Pre-pull configuration push

--- app.js
+++ app.js
@@ -22,17 +22,17 @@ var searchRoute = require('./routes/search_client');
 var complainRoute = require('./routes/complain');
 
 mongoose.connect(process.env.LOCALDB, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     dbName: 'swastik'
-  }).then(()=>{
-      console.log("db connected!!");
-  }).catch((e)=>{
-    console.log('Database connectivity error ',e)
-  });
+}).then(() => {
+    console.log("db connected!!");
+}).catch((e) => {
+    console.log('Database connectivity error ', e)
+});
 
 app.use(require("express-session")({
-    secret:"secret!",
+    secret: "secret!",
     resave: false,
     saveUninitialized: false
 }));
@@ -51,28 +51,28 @@ app.use("/public", express.static(__dirname + '/public'));
 app.use('/Register', registerRoute);
 app.use('/Login', loginRoute);
 app.use('/Signup', signupRoute);
-app.use('/Protected',protectRoute);
-app.use('/Client',clientRoute);
-app.use('/Message',messageRoute);
-app.use('/Statistics',statisticsRoute);
-app.use('/Search',searchRoute);
-app.use('/Complain',complainRoute);
+app.use('/Protected', protectRoute);
+app.use('/Client', clientRoute);
+app.use('/Message', messageRoute);
+app.use('/Statistics', statisticsRoute);
+app.use('/Search', searchRoute);
+app.use('/Complain', complainRoute);
 
 app.get('/Contacts', (req, res) => {
     res.render('contact');
 })
 
-app.get('/admin',(req,res)=>{
+app.get('/admin', (req, res) => {
     res.render("admin");
 });
-app.get('/dashboard',(req,res)=>{
+app.get('/dashboard', (req, res) => {
     res.render("dashboard");
 })
-app.get('/gallery',(req,res)=>{
+app.get('/gallery', (req, res) => {
     res.render("gallery");
 })
 
-app.get("/qrread",(req,res)=>{
+app.get("/qrread", (req, res) => {
     res.render("qrcode");
 });
 
@@ -81,42 +81,42 @@ app.get("/", function (req, res) {
     var female_parents = [];
     var male_childs = [];
     var female_childs = [];
-    Parent.find({},(err,parent)=>{
-        if(err){
+    Parent.find({}, (err, parent) => {
+        if (err) {
             console.log("error");
         }
-        else{
-            setTimeout(()=>{
-                parent.forEach((parent)=>{
-                    if(parent.gender == 'M'){
+        else {
+            setTimeout(() => {
+                parent.forEach((parent) => {
+                    if (parent.gender == 'M') {
                         male_parents.push(parent);
                     }
-                    else if(parent.gender == 'F'){
+                    else if (parent.gender == 'F') {
                         female_parents.push(parent);
                     }
                 })
-            },100); 
+            }, 100);
         }
     });
-    Child.find({},(err,child)=>{
-        if(err){
+    Child.find({}, (err, child) => {
+        if (err) {
             console.log("error");
         }
-        else{
-            setTimeout(()=>{
-                child.forEach((child)=>{
-                    if(child.gender == 'M'){
+        else {
+            setTimeout(() => {
+                child.forEach((child) => {
+                    if (child.gender == 'M') {
                         male_childs.push(child);
                     }
-                    else if(child.gender == 'F'){
+                    else if (child.gender == 'F') {
                         female_childs.push(child);
                     }
                 })
-            },100); 
+            }, 100);
         }
     });
 
-//count of children on the basis of vaccines(copied from stats.js) starts
+    //count of children on the basis of vaccines(copied from stats.js) starts
     var map = {};
     Child.find({}, (err, ward) => {
         if (err) {
@@ -124,11 +124,11 @@ app.get("/", function (req, res) {
         }
         else {
             setTimeout(() => {
-                ward.forEach((child)=>{
+                ward.forEach((child) => {
                     var vaccinations = child.vaccinations;
-                    vaccinations.forEach((data)=>{
-                        if(!map[data.disease]){
-                            map[data.disease]=0;
+                    vaccinations.forEach((data) => {
+                        if (!map[data.disease]) {
+                            map[data.disease] = 0;
                         }
                         map[data.disease]++;
                     })
@@ -138,20 +138,32 @@ app.get("/", function (req, res) {
             }, 100);
         }
     });
-//count of children on the basis of vaccines(copied from stats.js) ends
-    setTimeout(()=>{
+    //count of children on the basis of vaccines(copied from stats.js) ends
+    setTimeout(() => {
         var count_JSON = {
-            'Male Parents Count':male_parents.length,
-            'Female Parents Count':female_parents.length,
-            'Male Children Count':male_childs.length,
-            'Female Children Count':female_childs.length,
+            'Male Parents Count': male_parents.length,
+            'Female Parents Count': female_parents.length,
+            'Male Children Count': male_childs.length,
+            'Female Children Count': female_childs.length,
         }
         // res.send(count_JSON);
-        res.render("index.ejs",{Data:count_JSON,gdata:map});
-    },500)
+        var labls = [];
+        var dat = [];
+        for (let [key, value] of Object.entries(map)) {
+            labls.push(`${key}`);
+            dat.push(`${value}`)
+        }
+        var dt = [];
+        dat.forEach((x) => {
+            dt.push(Number(x));
+        });
+        console.log(dt);
+        console.log(labls);
+        res.render("index.ejs", { Data: count_JSON, labls: labls, dt: dt });
+    }, 500)
 })
 
-app.get("/logout",function(req,res){
+app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
 })
